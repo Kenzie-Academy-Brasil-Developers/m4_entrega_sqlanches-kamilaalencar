4c6734edb34a6b0021b00e9adc73e582385c8d62
Kamila Alencar <kamilaalencar1998@gmail.com.(none)>
2022-04-26 19:08:36 -0300

    feat: criando queries

--- crud.sql
+++ crud.sql
@@ -5,30 +5,81 @@
 
 -- 1)
 
-
+INSERT INTO
+  clientes (nome, lealdade)
+VALUES
+  ('Georgia', 0);
 
 -- 2)
 
-
+INSERT INTO
+  pedidos (status, cliente_id)
+VALUES
+  ('Recebido', 6);
 
 -- 3)
 
-
+INSERT INTO
+  produtos_pedidos (pedido_id, produto_id)
+VALUES
+  (6, 1),
+  (6, 2),
+  (6, 6),
+  (6, 8),
+  (6, 8);
 
 -- Leitura
 
 -- 1)
 
-
+SELECT
+  cl.id,
+  cl.nome,
+  cl.lealdade,
+  pd.id,
+  pd.status,
+  pd.cliente_id,
+  pr.id,
+  pr.nome,
+  pr.tipo,
+  pr."preço",
+  pr.pts_de_lealdade
+FROM
+  pedidos pd
+  INNER JOIN produtos_pedidos pp ON pd.id = pp.pedido_id
+  INNER JOIN produtos pr ON pp.produto_id = pr.id
+  INNER JOIN clientes cl ON pd.cliente_id = cl.id 
+WHERE
+  cl.id = 6 ; -- ou cl.nome = 'Georgia' 
 
 -- Atualização
 
 -- 1)
 
-
+UPDATE
+  clientes
+SET
+  lealdade = (
+    SELECT
+      SUM(pr.pts_de_lealdade)
+    FROM
+      pedidos pd
+      INNER JOIN produtos_pedidos pp ON pd.id = pp.pedido_id
+      INNER JOIN produtos pr ON pp.produto_id = pr.id
+      INNER JOIN clientes cl ON pd.cliente_id = cl.id
+    WHERE
+      cl.id = 6
+  )
+WHERE
+  id = 6;
 
 -- Deleção
 
 -- 1)
+
+DELETE FROM
+  clientes
+WHERE
+  nome = 'Marcelo' --ou id = 4;
 
 
--- selects.sql
+++ selects.sql
@@ -2,11 +2,61 @@
 -- Seleções de dados
 
 -- 1)
+SELECT
+  pd.id,
+  pd.status,
+  pd.cliente_id,
+  pr.id,
+  pr.nome,
+  pr.tipo,
+  pr."preço",
+  pr.pts_de_lealdade
+FROM
+  pedidos pd
+  INNER JOIN produtos_pedidos pp ON pd.id = pp.pedido_id
+  INNER JOIN produtos pr ON pp.produto_id = pr.id;
 
 -- 2)
+SELECT
+  pd.id
+FROM
+  pedidos pd
+  INNER JOIN produtos_pedidos pp ON pd.id = pp.pedido_id
+  INNER JOIN produtos pr ON pp.produto_id = pr.id
+WHERE
+  pr.id = 6; -- ou pr.nome = 'Fritas'
 
 -- 3)
+SELECT
+  cl.nome AS gostam_de_fritas
+FROM
+  pedidos pd
+  INNER JOIN produtos_pedidos pp ON pd.id = pp.pedido_id
+  INNER JOIN produtos pr ON pp.produto_id = pr.id
+  INNER JOIN clientes cl ON pd.cliente_id = cl.id
+WHERE
+  pr.id = 6; -- ou  pr.nome = 'Fritas'
 
 -- 4)
+SELECT
+  SUM(pr."preço")
+FROM
+  pedidos pd
+  INNER JOIN produtos_pedidos pp ON pd.id = pp.pedido_id
+  INNER JOIN produtos pr ON pp.produto_id = pr.id
+  INNER JOIN clientes cl ON pd.cliente_id = cl.id
+WHERE
+  cl.id = 5; -- ou cl.nome = 'Laura'
 
 -- 5)
+SELECT
+  pr.nome,
+  COUNT(pp.produto_id)
+FROM
+  pedidos pd
+  INNER JOIN produtos_pedidos pp ON pd.id = pp.pedido_id
+  INNER JOIN produtos pr ON pp.produto_id = pr.id
+GROUP BY
+  pr.nome
+ORDER BY
+  pr.nome;
